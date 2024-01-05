bb751f3ed78f317df31142df20183d39ad8dde83
domingasp <d.pleck@outlook.com>
2024-01-05 11:52:11 +0000

    adding Mantine, removing out files from VC

--- out/preload/preload.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const electron = require("electron");
-const channels = {
-  getOverlayHotkey: "getOverlayHotkey"
-};
-electron.contextBridge.exposeInMainWorld("hotkeyOverlaysAPI", {
-  [channels.getOverlayHotkey]: () => electron.ipcRenderer.invoke(channels.getOverlayHotkey)
-});
