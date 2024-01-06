fd0b10519bb6eb5a331e5e7f7338d261e6444934
domingasp <d.pleck@outlook.com>
2024-01-06 11:56:55 +0000

    reworking save button appearance

--- src/renderer/src/components/NameInput.tsx
+++ src/renderer/src/components/NameInput.tsx
@@ -8,11 +8,17 @@ type NameInputProps = {
 };
 function NameInput({ value, setValue }: NameInputProps) {
   const [isNameFieldFocused, setIsNameFieldFocused] = useState(false);
+  const [isSavingNameField, setIsSavingNameField] = useState(false);
+
   const [width, setWidth] = useState(0);
   const span = createRef<HTMLParagraphElement>();
 
   const saveNameField = () => {
+    setIsSavingNameField(true);
     console.log('Name saved');
+    setTimeout(() => {
+      setIsSavingNameField(false);
+    }, 2000);
   };
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -54,23 +60,23 @@ function NameInput({ value, setValue }: NameInputProps) {
         size="md"
         pl={0}
         onFocus={() => setIsNameFieldFocused(true)}
-        onBlur={() => setIsNameFieldFocused(false)}
+        onBlur={() => setTimeout(() => setIsNameFieldFocused(false), 100)}
+        disabled={isSavingNameField}
         rightSection={
-          <ActionIcon
-            variant="light"
-            color="green"
-            aria-label="Save Name"
-            onClick={saveNameField}
-            style={{
-              transition: 'visibility 0s 100ms',
-              visibility: isNameFieldFocused ? 'visible' : 'hidden',
-            }}
-          >
-            <IconCheck
-              pointerEvents="none"
-              style={{ width: rem(14), height: rem(14) }}
-            />
-          </ActionIcon>
+          (isNameFieldFocused || isSavingNameField) && (
+            <ActionIcon
+              variant="light"
+              color="green"
+              aria-label="Save Name"
+              onClick={saveNameField}
+              loading={isSavingNameField}
+            >
+              <IconCheck
+                pointerEvents="none"
+                style={{ width: rem(14), height: rem(14) }}
+              />
+            </ActionIcon>
+          )
         }
         rightSectionPointerEvents="all"
         rightSectionWidth="36px"
